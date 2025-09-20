39ffafcdf316ebb9eb5b16339548ef03bf864970
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-19 22:01:31 -0400

    js exercise 09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        sum += i;
+    } 
+    return sum;
 };
 
 // Do not edit below this line
