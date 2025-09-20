84ca540e0e85242751dad5c2cb066a575cfd93f4
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-19 22:25:20 -0400

    js exercise 11 done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const convertedToCelsius = (fahrenheit - 32) * 5/9;
+  return Math.round(convertedToCelsius * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const convertedToFahrenheit = (celsius * 9/5) + 32;
+  return Math.round(convertedToFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
