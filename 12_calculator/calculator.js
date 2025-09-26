15c77afc5b85c0c61518b39759005d590102609e
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-26 19:43:22 -0400

    js exercise 12 done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let ans = 0;
+	for (let i in arr) {
+    ans += i;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let ans = 1
+  for (let i = n; i > 0; i--) {
+    ans *= i;
+  }
 };
 
 // Do not edit below this line
