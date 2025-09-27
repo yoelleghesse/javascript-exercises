330e26db9d4139d5c4391545911bad1e9f2e6fa3
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-26 21:50:30 -0400

    js exercise 15 done

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (let i in books) {
+        const book = books[i]
+        titles.push(book.title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
