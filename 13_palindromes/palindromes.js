b88146d609ef2a94670af6e84779b48f160178ea
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-26 20:25:21 -0400

    js exercise 13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str.split("").reverse().join("");
+    return str.replace(/[^\p{L}]/gu, "");
 };
 
 // Do not edit below this line
