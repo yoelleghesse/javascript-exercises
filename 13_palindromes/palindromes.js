513e677f1534e83bcb0860f4534f687517aef908
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-26 20:48:38 -0400

    js exercise 14 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
 const palindromes = function (str) {
-    str.split("").reverse().join("");
-    return str.replace(/[^\p{L}]/gu, "");
+    const strS = str.replace(/[^\p{L}]/gu, "").toLowerCase();
+    const r_str = strS.split("").reverse().join("");
+
+    if (strS === r_str) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    fib = 0
+    for (let i = 1; i < n; i ++) {
+        fib += 1;
+    }
+    return fib 
 };
 
 // Do not edit below this line
