16d275eddf7e0bd17e34cd9522de3bdcd6af0d13
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-26 22:45:41 -0400

    js exercise 16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let currentYear = new Date().getFullYear();
 
+  // Compute age for each person and keep reference to person
+  let oldestPerson = array.reduce((oldest, person) => {
+    let age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    return age > oldest.age ? { person, age } : oldest;
+  }, { person: null, age: 0 });
+
+  return oldestPerson.person;
 };
 
 // Do not edit below this line
