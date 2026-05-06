dddd843f499c8faafcff7376c9b439c696b5791c
Yoel Leghesse <yoelleghesse@gmail.com>
2026-05-05 21:27:48 -0400

    05 js done

--- foundations/05_joinStrings/joinStrings-example.js
+++ foundations/05_joinStrings/joinStrings-example.js
@@ -2,15 +2,18 @@ const birthYear = 1947;
 const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
-console.log(greeting);
 
 module.exports = {
   greeting,
   birthYear,
   thisYear,
   firstName,
-  lastName
+  lastName,
+  fullName,
+  age,
 }
--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -6,8 +6,14 @@
 
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
-// Add your code right below, good luck!
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
 
 
